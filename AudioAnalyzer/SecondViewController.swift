f28f315a2f59689226523003451faf1eaf391d5f
JuanS <jdsierral@hotmail.com>
2017-11-28 15:45:38 -0800

    Initial Commit

---
+++ AudioAnalyzer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AudioAnalyzer
+//
+//  Created by Juan David Sierra on 11/28/17.
+//  Copyright © 2017 JuanSaudio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
